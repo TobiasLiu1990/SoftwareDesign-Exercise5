ecd1f863dd91ad0d2a43ec6a590768bc5bdcb92a
Mayonnaisu <toli025@student.kristiania.no>
2022-10-02 16:21:07 +0200

    Add project files.

---
+++ Fractions/Fraction.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+public class Fraction
+{
+    public int Numerator { get; set; } = 0;
+    public int Denominator { get; set; } = 1;
+
+    public Fraction(int numerator, int denominator)
+    {
+        Numerator = numerator;
+
+        if (Denominator > 0) {
+            Denominator = denominator;
+        }
+        else {
+            throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    public Fraction Multiplication(Fraction input)
+    {
+        int numeratorResult = Numerator * input.Numerator;
+        int denominatorResult = Denominator * input.Denominator;
+
+        return new Fraction(numeratorResult, denominatorResult);
+    }
+
+    public Fraction Addition(Fraction input)
+    {
+        int finalResult;
+        int commonDenominator = Denominator;
+
+        if (Denominator == input.Denominator) {
+            finalResult = Numerator + input.Numerator;
+        } 
+        else {
+            commonDenominator = Denominator * input.Denominator;
+            int resultNumeratorOne = Numerator * input.Denominator;
+            int resultNumeratorTwo = input.Numerator * Denominator;
+
+            finalResult = resultNumeratorOne + resultNumeratorTwo;
+        }
+        return new Fraction(finalResult, commonDenominator);
+    }
+
+    public Fraction Subtraction(Fraction input)
+    {
+        int finalResult;
+        int commonDenominator = Denominator;
+
+        if (Denominator == input.Denominator) {
+            finalResult = Numerator - input.Numerator;
+        }
+        else {
+            commonDenominator = Denominator * input.Denominator;
+            int resultNumeratorOne = Numerator * input.Denominator;
+            int resultNumeratorTwo = input.Numerator * Denominator;
+
+            finalResult = resultNumeratorOne - resultNumeratorTwo;
+        }
+        return new Fraction(finalResult, commonDenominator);
+    }
+
+    public Fraction Division(Fraction input)
+    {
+        int inputNewNumerator = Numerator * input.Denominator;
+        int inputNewDenominator = Denominator * input.Numerator;
+
+        return Reduce(new Fraction(inputNewNumerator, inputNewDenominator));
+    }
+
+    public Fraction Reduce(Fraction input)
+    {
+        int biggestNumber = Math.Min(input.Numerator, input.Denominator);
+
+        while (biggestNumber > 1) {
+            if (input.Numerator % biggestNumber == 0 && input.Denominator % biggestNumber == 0) {
+                input.Numerator /= biggestNumber;
+                input.Denominator /= biggestNumber;
+                biggestNumber = Math.Min(input.Numerator, input.Denominator);
+            }
+            else {
+                biggestNumber--;
+            }
+        }
+        return new Fraction(input.Numerator, input.Denominator);
+    }
+
+
+    public bool GreaterThan(Fraction fraction)
+    {
+
+        return true;
+    }
+
+    public bool LesserThan(Fraction fraction)
+    {
+        return true;
+    }
+
+
+    //public override bool Equals(Fraction fraction)
+    //{
+    //    return base.Equals(fraction.Numerator / fraction.Denominator);
+    //}
+
+    public override string ToString()
+    {
+        return "Numerator: " + Numerator + "\n" + "Denominator: " + Denominator + "\n";
+    }
+
+
+    //Properties
+
+
+}
---
+++ Fractions/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Fractions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction fraction = new Fraction(1, 2);
+            Fraction fraction2 = new Fraction(2, 3);
+
+            Console.WriteLine("Multiply: " + fraction.Multiplication(fraction2));
+            Console.WriteLine("Add: " + fraction.Addition(fraction2));
+            Console.WriteLine("Subtract: " + fraction.Subtraction(fraction2));
+            Console.WriteLine("Divide: " + fraction.Division(fraction2));
+
+
+            Fraction reducedFraction = new Fraction(9, 21);
+            reducedFraction = reducedFraction.Reduce(reducedFraction);
+
+            Console.WriteLine("---Reduced fraction---: " + reducedFraction);
+        }
+    }
+}
